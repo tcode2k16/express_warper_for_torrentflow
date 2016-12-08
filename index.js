5f5da9633a84e72d90045334d0cc070875197c3e
tcode2k16 <alanc@macbook.local>
2016-12-08 12:37:28 +0800

    added api

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
+const thepiratebay = require('thepiratebay');
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use(express.static('public'));
+const allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
 
-app.listen(8080);
+    next()
+}
+
+// app.use(allowCrossDomain)
+
+
+app.use(express.static('public'))
+
+app.post('/api/search', bodyParser.urlencoded({ extended: false }), (req, res) => {
+  if (req.body.search == undefined) {
+    res.send("not found")
+    return
+  }
+  thepiratebay.search(req.body.search)
+  .then(results => {
+    res.send(results)
+  })
+  .catch(err => {
+    res.send(err)
+  })
+})
+
+app.listen(8080)
